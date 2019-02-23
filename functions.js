f3a3a68238f4e9667b72c6cb8795996fd9f0c2b9
LittleBelka <Kompp@yandex.ru>
2019-02-24 01:38:15 +0300

    add: drop-down menu

---
+++ functions.js
@@ -0,0 +1,8 @@
+console.log("I'm heeeeee  111");
+
+var hoverOrClick = function () {
+	console("I'm heeeeee");
+    alert("I'm here");
+
+}
+$('#changeLanguage').click(hoverOrClick).hover(hoverOrClick);
